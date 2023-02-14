d4b49fc2247362f3920a4f8c6cbfda9fc630604e
jszfree <2676656238@qq.com>
2023-02-14 16:10:06 +0800

    little change

--- cmd/api/biz/apimodel/object.go
+++ cmd/api/biz/apimodel/object.go
@@ -8,7 +8,7 @@ import (
 )
 
 type User struct {
-	UserID        int64  `form:"user_id" json:"user_id" query:"user_id"`
+	UserID        int64  `form:"user_id" json:"id" query:"user_id"`
 	Username      string `form:"name" json:"name" query:"name"`
 	FollowCount   int64  `form:"follow_count" json:"follow_count" query:"follow_count"`
 	FollowerCount int64  `form:"follower_count" json:"follower_count" query:"follower_count"`
