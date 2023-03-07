7cd92f65f2a5a64d395a45e34bcd8e9b20ac219b
hcq <hanchaoqi1@126.com>
2023-03-07 10:49:29 +0800

    异步处理

--- test_benchmark/favorite_test.go
+++ test_benchmark/favorite_test.go
@@ -8,23 +8,23 @@ import (
 
 func BenchmarkFavoriteAct(b *testing.B) {
 	e := newExpect(b)
-	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzcyODk4ODIsImlkIjoxMjEsIm9yaWdfaWF0IjoxNjc3MjAzNDgyfQ.Lkuj8M7p7P1Zg-XqFpNpff1NBrioEHeBEZGVFHbUmkQ"
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzgyNDMwNDEsImlkIjoxNCwib3JpZ19pYXQiOjE2NzgxNTY2NDF9.MiShRh6jvx9XQOxoVA1YqB0I8UM-W9DuGO3-2Za7EwI"
 	_ = e.POST("/douyin/favorite/action/").
-		WithQuery("token", token).WithQuery("video_id", "545").WithQuery("action_type", 1).
-		WithFormField("token", token).WithFormField("video_id", "545").WithFormField("action_type", 1).
+		WithQuery("token", token).WithQuery("video_id", "1").WithQuery("action_type", 1).
+		WithFormField("token", token).WithFormField("video_id", "1").WithFormField("action_type", 1).
 		Expect().
 		Status(http.StatusOK).
 		JSON().Object()
 	_ = e.POST("/douyin/favorite/action/").
-		WithQuery("token", token).WithQuery("video_id", "545").WithQuery("action_type", 2).
-		WithFormField("token", token).WithFormField("video_id", "545").WithFormField("action_type", 2).
+		WithQuery("token", token).WithQuery("video_id", "1").WithQuery("action_type", 2).
+		WithFormField("token", token).WithFormField("video_id", "1").WithFormField("action_type", 2).
 		Expect().
 		Status(http.StatusOK).
 		JSON().Object()
 }
 func BenchmarkGetFavoriteList(b *testing.B) {
 	e := newExpect(b)
-	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzcyODk4ODIsImlkIjoxMjEsIm9yaWdfaWF0IjoxNjc3MjAzNDgyfQ.Lkuj8M7p7P1Zg-XqFpNpff1NBrioEHeBEZGVFHbUmkQ"
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzgyNDMwNDEsImlkIjoxNCwib3JpZ19pYXQiOjE2NzgxNTY2NDF9.MiShRh6jvx9XQOxoVA1YqB0I8UM-W9DuGO3-2Za7EwI"
 	_ = e.GET("/douyin/favorite/list/").
 		WithQuery("token", token).WithQuery("user_id", "121").
 		WithFormField("token", token).WithFormField("user_id", "121").
@@ -32,4 +32,3 @@ func BenchmarkGetFavoriteList(b *testing.B) {
 		Status(http.StatusOK).
 		JSON().Object()
 }
-
--- test_benchmark/message_test.go
+++ test_benchmark/message_test.go
@@ -7,18 +7,18 @@ import (
 
 func BenchmarkChat(b *testing.B) {
 	e := newExpect(b)
-	token1 := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzcyODk4ODIsImlkIjoxMjEsIm9yaWdfaWF0IjoxNjc3MjAzNDgyfQ.Lkuj8M7p7P1Zg-XqFpNpff1NBrioEHeBEZGVFHbUmkQ"
+	token1 := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzgyNDMwNDEsImlkIjoxNCwib3JpZ19pYXQiOjE2NzgxNTY2NDF9.MiShRh6jvx9XQOxoVA1YqB0I8UM-W9DuGO3-2Za7EwI"
 	//token2 := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzcyOTQxMjgsImlkIjoxMzAsIm9yaWdfaWF0IjoxNjc3MjA3NzI4fQ.Xy2iBkUH-JLDIGzcpYBmcYe01yxiP9DQ-hAQAnjxNig"
 	_ = e.POST("/douyin/message/action/").
-	WithQuery("token", token1).WithQuery("to_user_id", "130").WithQuery("action_type", 1).WithQuery("content", "Send to UserB").
-	WithFormField("token", token1).WithFormField("to_user_id", "130").WithFormField("action_type", 1).WithQuery("content", "Send to UserB").
-	Expect().
-	Status(http.StatusOK).
-	JSON().Object()
+		WithQuery("token", token1).WithQuery("to_user_id", "130").WithQuery("action_type", 1).WithQuery("content", "Send to UserB").
+		WithFormField("token", token1).WithFormField("to_user_id", "130").WithFormField("action_type", 1).WithQuery("content", "Send to UserB").
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object()
 }
 func BenchmarkGetMessageList(b *testing.B) {
 	e := newExpect(b)
-	token1 := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzcyODk4ODIsImlkIjoxMjEsIm9yaWdfaWF0IjoxNjc3MjAzNDgyfQ.Lkuj8M7p7P1Zg-XqFpNpff1NBrioEHeBEZGVFHbUmkQ"
+	token1 := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzgyNDMwNDEsImlkIjoxNCwib3JpZ19pYXQiOjE2NzgxNTY2NDF9.MiShRh6jvx9XQOxoVA1YqB0I8UM-W9DuGO3-2Za7EwI"
 	//token2 := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzcyOTQxMjgsImlkIjoxMzAsIm9yaWdfaWF0IjoxNjc3MjA3NzI4fQ.Xy2iBkUH-JLDIGzcpYBmcYe01yxiP9DQ-hAQAnjxNig"
 	_ = e.GET("/douyin/message/chat/").
 		WithQuery("token", token1).WithQuery("to_user_id", "130").
